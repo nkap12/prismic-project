8a62239e7fd917177b75a546beb5271c53efac20
nkap12 <nicholekaplinsky@gmail.com>
2022-11-18 12:06:11 +0000

    tailwind page demo

--- pages/tailwind.js
+++ pages/tailwind.js
@@ -3,8 +3,18 @@
 
 export default function Home() {
     return (
-      <h1 className="text-3xl text-pink-300 font-bold underline ">
-        Hello world!
-      </h1>
-    )
+      <div>
+        <body >
+          <h1 className=" bg-pink-400 text-3xl text-pink-300 font-bold underline ">
+            Hello world!
+          </h1>
+          <div className="p-20 flex items-center gap-6 justify-center bg-gray-100">
+            <div className="p-10 border border-pink-400 bg-pink-300">1</div>
+            <div className="p-10 border border-pink-400 bg-pink-300">2</div>
+            <div className="p-10 border border-pink-400 bg-pink-300">3</div>
+            <button className="bg-pink-400 p-3 rounded hover:bg-blue-200" >Click me!</button>
+          </div>
+        </body>
+      </div>
+    );
   }
